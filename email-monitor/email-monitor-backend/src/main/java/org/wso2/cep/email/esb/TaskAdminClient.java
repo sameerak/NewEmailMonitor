cac4c61bc129b1dfb06609d2300032b181e6ccdd
sacjaya <sachinijayasekara.g@gmail.com>
2014-05-28 10:51:51 +0530

    Admin clients

---
+++ email-monitor/email-monitor-backend/src/main/java/org/wso2/cep/email/esb/TaskAdminClient.java
@@ -0,0 +1,6 @@
+package org.wso2.cep.email.esb;
+
+
+public class TaskAdminClient {
+//    TaskAdminClient
+}
---
+++ email-monitor/email-monitor-backend/src/main/java/org/wso2/cep/email/esb/util/SecurityConstants.java
@@ -0,0 +1,18 @@
+package org.wso2.cep.email.esb.util;
+
+
+import org.wso2.carbon.utils.ServerConstants;
+
+import java.io.File;
+
+public class SecurityConstants {
+    public static final String KEY_STORE_PASSWORD = "wso2carbon";
+    public static final String KEY_STORE_TYPE = "jks";
+    public static final String TRUSTSTORE= "javax.net.ssl.trustStore";
+    public static final String TRUSTSTORE_PASSWORD= "javax.net.ssl.trustStorePassword";
+    public static final String TRUSTSTORE_TYPE = "javax.net.ssl.trustStoreType";
+
+    public static final String CLIENT_TRUST_STORE_PATH = System.getProperty(ServerConstants.CARBON_HOME)+ File.separator+"repository"+File.separator+"resources"+File.separator+"security"+ File.separator+"client-truststore.jks";
+
+}
+
