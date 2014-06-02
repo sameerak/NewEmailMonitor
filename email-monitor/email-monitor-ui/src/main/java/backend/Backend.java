93be50c81df5eeef9ac65ff85cff3a5e211cb48c
sacjaya <sachinijayasekara.g@gmail.com>
2014-06-02 10:21:16 +0530

    UI backend

--- email-monitor/email-monitor-ui/src/main/java/backend/Backend.java
+++ email-monitor/email-monitor-ui/src/main/java/backend/Backend.java
@@ -1,33 +1,17 @@
 package backend;
 
-import org.wso2.carbon.task.stub.TaskManagementException;
-import org.wso2.cep.email.esb.*;
+/**
+ * Created by sameerak on 5/28/14.
+ */
 
-import javax.xml.stream.XMLStreamException;
-import java.rmi.RemoteException;
+import org.wso2.cep.email.esb.ProxyAdminClient;
 
 public class Backend {
 
     public static String test(String ip, String port){
         String out = "false";
-
-        BAMMediatorAdminClient bamMediatorAdminClient = new BAMMediatorAdminClient(ip, port);
-        bamMediatorAdminClient.addMailProxy("admin", "admin");
-
         ProxyAdminClient proxyAdminClient = new ProxyAdminClient(ip, port);
         proxyAdminClient.addMailProxy("admin", "admin");
-
-        TaskAdminClient taskAdminClient = new TaskAdminClient(ip, port);
-        try {
-            taskAdminClient.addScheduledTask("admin", "admin");
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        } catch (TaskManagementException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-
         out = "true";
         return out;
     }
