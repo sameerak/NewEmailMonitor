a56a734107df7716308f6cff958fb5196a9859dc
sacjaya <sachinijayasekara.g@gmail.com>
2014-06-10 16:45:40 +0530

    addinf CEP configuration helper

--- email-monitor/email-monitor-backend/src/main/java/org/wso2/cep/email/monitor/internal/config/EventFormatterDeployer.java
+++ email-monitor/email-monitor-backend/src/main/java/org/wso2/cep/email/monitor/internal/config/EventFormatterDeployer.java
@@ -1,8 +1,6 @@
 package org.wso2.cep.email.monitor.internal.config;
 
 
-
-import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.log4j.Logger;
 import org.wso2.carbon.event.formatter.core.EventFormatterService;
@@ -15,31 +13,28 @@ import org.wso2.cep.email.monitor.internal.util.EmailMonitorConstants;
 
 public class EventFormatterDeployer {
     private static Logger logger = Logger.getLogger(EventFormatterDeployer.class);
-    private EventFormatterService eventFormatterService ;
-    private EmailMonitorValueHolder emailMonitorValueHolder;
+    private EventFormatterService eventFormatterService;
     private XMLReader xmlReader;
 
     public EventFormatterDeployer() throws EmailMonitorServiceException {
 
-        emailMonitorValueHolder = EmailMonitorValueHolder.getInstance();
-        eventFormatterService = emailMonitorValueHolder.getEventFormatterService() ;
+        EmailMonitorValueHolder emailMonitorValueHolder = EmailMonitorValueHolder.getInstance();
+        eventFormatterService = emailMonitorValueHolder.getEventFormatterService();
         xmlReader = new XMLReader();
     }
 
 
-
-
     public void createGmailOutStreamEventFormatter(String ESBServerIP, String ESBServerPort, String ESBServerUsername, String ESBServerPassword, AxisConfiguration axisConfiguration) throws EmailMonitorServiceException {
         String content = xmlReader.readXML(EmailMonitorConstants.GMAIL_OUT_STREAM_CONFIGURATION_PATH);
-        content = content.replace(EmailMonitorConstants.ESB_SERVER_USER_NAME,ESBServerUsername);
-        content = content.replace(EmailMonitorConstants.ESB_SERVER_PASSWORD,ESBServerPassword);
-        content = content.replace(EmailMonitorConstants.ESB_SERVER_IP,ESBServerIP);
+        content = content.replace(EmailMonitorConstants.ESB_SERVER_USER_NAME, ESBServerUsername);
+        content = content.replace(EmailMonitorConstants.ESB_SERVER_PASSWORD, ESBServerPassword);
+        content = content.replace(EmailMonitorConstants.ESB_SERVER_IP, ESBServerIP);
 
         int offset = Integer.parseInt(ESBServerPort) - 9443;
-        content = content.replace(EmailMonitorConstants.ESB_SERVER_ENDPOINT_PORT,String.valueOf(8243+offset));
+        content = content.replace(EmailMonitorConstants.ESB_SERVER_ENDPOINT_PORT, String.valueOf(8243 + offset));
 
         try {
-            eventFormatterService.deployDefaultEventSender(content,axisConfiguration);
+            eventFormatterService.deployDefaultEventSender(content, axisConfiguration);
         } catch (EventFormatterConfigurationException e) {
             logger.error(e.getMessage());
             throw new EmailMonitorServiceException("Error when adding event formatter", e);
@@ -49,14 +44,14 @@ public class EventFormatterDeployer {
     }
 
 
-    public void createEmailSenderOutputStreamFormatter(String mailBody, String mailAddress, String mailSubject, AxisConfiguration axisConfiguration) throws EmailMonitorServiceException {
+    public void createEmailSenderOutputStreamFormatter( String mailAddress, AxisConfiguration axisConfiguration) throws EmailMonitorServiceException {
         String content = xmlReader.readXML(EmailMonitorConstants.MAIL_SENDER_OUT_STREAM_CONFIGURATION_PATH);
-        content = content.replace(EmailMonitorConstants.MAIL_BODY, mailBody);
-        content = content.replace(EmailMonitorConstants.GMAIL_USERNAME,mailAddress);
-        content = content.replace(EmailMonitorConstants.MAIL_SUBJECT, mailSubject);
+        content = content.replace(EmailMonitorConstants.MAIL_BODY, "Details:");
+        content = content.replace(EmailMonitorConstants.GMAIL_USERNAME, mailAddress);
+        content = content.replace(EmailMonitorConstants.MAIL_SUBJECT, "[CEP]Important email alert ");
 
         try {
-            eventFormatterService.deployDefaultEventSender(content,axisConfiguration);
+            eventFormatterService.deployDefaultEventSender(content, axisConfiguration);
         } catch (EventFormatterConfigurationException e) {
             logger.error(e.getMessage());
             throw new EmailMonitorServiceException("Error when adding event formatter", e);
