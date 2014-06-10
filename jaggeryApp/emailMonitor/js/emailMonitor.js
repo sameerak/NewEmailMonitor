29ac22093dac4777f9bf2efc7274b33070d46f35
sameerak <dlskannangara@gmail.com>
2014-06-10 15:35:15 +0530

    changing js code to call query adding functionality separately

--- jaggeryApp/emailMonitor/js/emailMonitor.js
+++ jaggeryApp/emailMonitor/js/emailMonitor.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		
 		 // fire off the request to /api
 		 request = $.ajax({
-		     url: "api",
+		     url: "config",
 		     type: "post",
 		     data: serializedData
 		 });
@@ -30,7 +30,7 @@ $(document).ready(function() {
 		 request.done(function (response, textStatus, jqXHR){
 		     // log a message to the console
 			 bootbox.alert(response);
-		     console.log("Hooray, it worked!");
+//		     console.log("Hooray, it worked!");
 		 });
 		
 		 // callback handler that will be called on failure
@@ -41,6 +41,58 @@ $(document).ready(function() {
 		         textStatus, errorThrown
 		     );
 		     bootbox.alert("Please fill all parameters!");
+		 });
+		
+		 // callback handler that will be called regardless
+		 // if the request failed or succeeded
+		 request.always(function () {
+		     // reenable the inputs
+		     $inputs.prop("disabled", false);
+		 });
+		
+		 // prevent default posting of form
+		 event.preventDefault();
+	});
+	
+	$("#emailMonitorQueryForm").submit(function(event){
+		// abort any pending request
+		 if (request) {
+		     request.abort();
+		 }
+		 // setup some local variables
+		 var $form = $(this);
+		 // let's select and cache all the fields
+		 var $inputs = $form.find("input, select, button, textarea");
+		 // serialize the data in the form
+		 var serializedData = $form.serialize();
+		
+		 // let's disable the inputs for the duration of the ajax request
+		 // Note: we disable elements AFTER the form data has been serialized.
+		 // Disabled form elements will not be serialized.
+		 $inputs.prop("disabled", true);
+		
+		 // fire off the request to /api
+		 request = $.ajax({
+		     url: "query",
+		     type: "post",
+		     data: serializedData
+		 });
+		
+		 // callback handler that will be called on success
+		 request.done(function (response, textStatus, jqXHR){
+		     // log a message to the console
+			 bootbox.alert(response);
+//		     console.log("Hooray, it worked!");
+		 });
+		
+		 // callback handler that will be called on failure
+		 request.fail(function (jqXHR, textStatus, errorThrown){
+		     // log the error to the console
+		     console.error(
+		         "The following error occured: "+
+		         textStatus, errorThrown
+		     );
+		     bootbox.alert("Please configure email monitor with correct details!");
 		 });
 		
 		 // callback handler that will be called regardless
